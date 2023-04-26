42b4f88af471e7c088b460d0d3a643b62530b069
sukanya <sukanyasirigiri@gmail.com>
2023-04-26 12:12:39 -0400

    varfiles

--- 1-vars/outputs.tf
+++ 1-vars/outputs.tf
@@ -19,3 +19,11 @@ output "sample_dict_num1" {
 output "sample_dict_boolean" {
     value = var.sample_dict["boolean"]
 }
+output "env" {
+    value = var.env
+
+}
+
+output "auto_num1" {
+    value = var.auto_num1
+}
