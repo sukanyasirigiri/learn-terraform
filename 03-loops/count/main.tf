34630c081ad2ffd557e50ecc5546f0dafc9213fc
sukanya <sukanyasirigiri@gmail.com>
2023-04-26 16:09:00 -0400

    loops

--- 03-loops/count/main.tf
+++ 03-loops/count/main.tf
@@ -1,3 +1,9 @@
 resource "null_resource" "null" {
   count = 10
 }
+resource "null_resource" "name" {
+  count = length(var.fruits)
+}
+variable = "fruits" {
+    default = [ "apple", "banana", "orange" ]
+}
