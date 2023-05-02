c0914c75df514af92fdb57864b12e5c5fd484f21
sukanya <sukanyasirigiri@gmail.com>
2023-05-02 15:31:01 -0400

    foreach

---
+++ for-each/main.tf
@@ -0,0 +1,14 @@
+resource "null_resource" "fruits" {
+    for_each=var.fruits
+    provisioner "local_exe" {
+    command = "echo fruit name -${each.key} - ${each.value}"
+    }
+}
+
+variable "fruits" {
+  default = {
+    apple = 10
+    banan = 20
+    orange = 30
+  }
+}
