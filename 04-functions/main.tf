1d323230020d1e0aa5c0354b47f98d7416575888
sukanya <sukanyasirigiri@gmail.com>
2023-05-03 18:08:40 -0400

    functions

---
+++ 04-functions/main.tf
@@ -0,0 +1,7 @@
+# convert lower case to upper case
+ variable "sample"{
+    default = "abcdef"
+ }
+ output "sample" {
+    value = "var.sample"
+ }
--- for-each/main.tf
+++ for-each/main.tf
@@ -14,3 +14,4 @@ variable "fruits" {
     orange = 30
   }
 }
+
