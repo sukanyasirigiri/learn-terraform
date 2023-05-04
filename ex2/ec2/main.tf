7a324ceabbd41e3f1f4509daab048c4cd7745ec7
sukanya <sukanyasirigiri@gmail.com>
2023-05-04 18:35:43 -0400

    module

---
+++ ex2/ec2/main.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "instance" {
+  for_each = var.components
+  ami           = data.aws_ami.centos.image_id
+
+  instance_type = "t3.micro"
+vpc_security_group_ids = [ var.sg_id ]
+ 
+  tags = {
+    Name = "test"
+  }
+} 
+
+data "aws_ami" "centos" {
+  owners = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+}
+
+variable "sg_id" {}
